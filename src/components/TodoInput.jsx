f29dd66356c083065f280cf9d602fcdaa8e042ba
Arth Mohan <writetoarth@gmail.com>
2024-09-13 03:30:56 +0530

    maybe fixed enter issue?

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -8,19 +8,21 @@ export default function TodoInput(props) {
       <input 
         value={todoValues} 
         onChange={(e) => setTodoValues(e.target.value)} 
+        type="text" 
+        placeholder="Enter To-Do..." 
+      />
+      <button 
         onKeyDown={(e) => {
           if (e.key === 'Enter') {
             handleAddTodos(todoValues)
             setTodoValues('')
           }
+        }} 
+        onClick={() => {
+          handleAddTodos(todoValues)
+          setTodoValues('')
         }}
-        type="text" 
-        placeholder="Enter To-Do..." 
-      />
-      <button  onClick={() => {
-        handleAddTodos(todoValues)
-        setTodoValues('')
-      }}>
+      >
         Add
       </button>
     </header>
